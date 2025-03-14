06c52dcf234fa52a2e02bcf23c4fd14b0645adf6
Easymean1207 <dlwlasl1207@gmail.com>
2025-03-14 16:07:21 +0900

    해시테이블 구현 코드 추가

---
+++ HashTable/hash_table.mjs
@@ -0,0 +1,82 @@
+import { DoublyLinkedList } from '../LinkedList/doubly_linked_list.mjs';
+
+class HashData {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
+class HashTable {
+  constructor() {
+    this.arr = [];
+    for (let i = 0; i < 10; i++) {
+      this.arr[i] = new DoublyLinkedList();
+    }
+  }
+
+  /**
+   * 해시 함수
+   * @param {number} input
+   * @returns {number} hashIndex
+   */
+  hashFunction(input) {
+    return input % 10;
+  }
+
+  /**
+   * 해시 테이블에 데이터 삽입
+   * @param {number} key
+   * @param {any} value
+   */
+  set(key, value) {
+    // 해시함수의 결과값을 인덱스로 하는 연결리스트의 head에 데이터 삽입
+    this.arr[this.hashFunction(key)].insertAt(0, new HashData(key, value));
+  }
+
+  /**
+   * 해시 테이블에서 key에 해당하는 데이터 조회
+   * @param {number} key
+   * @returns {any} key에 해당하는 데이터
+   */
+  get(key) {
+    // 해시함수의 결과값을 인덱스로 하는 연결리스트의 head를 현재 노드로 설정
+    let currentNode = this.arr[this.hashFunction(key)].head;
+
+    // currentNode로 연결리스트를 순회하며 key에 해당하는 value를 찾음
+    while (currentNode) {
+      if (currentNode.data.key == key) {
+        return currentNode.data.value;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
+   * 해시 테이블에서 key에 해당하는 데이터 삭제
+   * @param {number} key
+   * @returns {Node} 삭제된 데이터
+   */
+  remove(key) {
+    // 해당 key의 인덱스 연결리스트를 가져옴
+    let list = this.arr[this.hashFunction(key)];
+    // 순회용 currentNode 설정
+    let currentNode = list.head;
+    // 연결리스트에서 삭제할 데이터의 인덱스 저장
+    let deletedIndex = 0;
+
+    // 연결리스트를 순회하며 key에 해당하는 연결리스트의 인덱스를 찾아 삭제
+    while (currentNode) {
+      if (currentNode.data.key == key) {
+        return list.deleteAt(deletedIndex);
+      }
+      currentNode = currentNode.next;
+      deletedIndex++;
+    }
+    return null;
+  }
+}
+
+export { HashTable };
---
+++ HashTable/test.mjs
@@ -0,0 +1,27 @@
+import { HashTable } from './hash_table.mjs';
+
+let hashTable = new HashTable();
+
+hashTable.set(1, '이운재');
+hashTable.set(4, '최진철');
+hashTable.set(20, '홍명보');
+hashTable.set(6, '유성철');
+hashTable.set(22, '송종국');
+hashTable.set(21, '박지성');
+hashTable.set(5, '김남일');
+hashTable.set(10, '이영표');
+hashTable.set(8, '최태욱');
+hashTable.set(9, '설기현');
+hashTable.set(14, '이천수');
+
+// console.log(hashTable.get(4)); // 최진철
+// hashTable.remove(4);
+// console.log(hashTable.get(14)); // 이천수
+
+for (let i = 0; i < hashTable.arr.length; i++) {
+  let currentNode = hashTable.arr[i].head;
+  while (currentNode) {
+    console.log(currentNode.data.key, currentNode.data.value);
+    currentNode = currentNode.next;
+  }
+}
